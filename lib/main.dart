8e20e653d301b6658a3fe75ffcccf5900010d49e
MichailoZaichenko <michailo.zaichenko@gmail.com>
2025-08-30 14:08:19 +0300

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,35 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Gym App'),
           centerTitle: true,
-          leading: Icon(Icons.login),
-          actions: [Text('adf'), Icon(Icons.login)],
-          backgroundColor: Colors.teal,
+          // leading: Icon(Icons.login),
+          // actions: [Text('adf'), Icon(Icons.login)],
+          // backgroundColor: Colors.teal,
+        ),
+        drawer: SafeArea(
+          child: Drawer(
+            child: Column(
+              children: [
+                DrawerHeader(child: Text('Drawer')),
+                ListTile(title: Text('Logout')),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+            SizedBox(height: 10.0),
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          onDestinationSelected: (int value) {},
+          selectedIndex: 1,
         ),
       ),
     );
