876cdf03d9a5fbb02f6a4d607a640edd2e958072
MichailoZaichenko <michailo.zaichenko@gmail.com>
2025-09-05 22:04:34 +0300

    Scroller

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -14,69 +14,78 @@ class _ProfilePageState extends State<ProfilePage> {
   double sliderValue = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Enter your name',
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your name',
+              ),
+              onEditingComplete: () {
+                setState(() {});
+              },
             ),
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(controller.text),
-          Checkbox.adaptive(
-            tristate: true,
-            value: isChenked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChenked = value;
-              });
-            },
-          ),
-          CheckboxListTile.adaptive(
-            tristate: true,
-            title: Text('Click me'),
-            value: isChenked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChenked = value;
-              });
-            },
-          ),
-          Switch.adaptive(
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          SwitchListTile.adaptive(
-            title: Text('Switch me'),
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          Slider.adaptive(
-            min: 0,
-            max: 100,
-            divisions: 10,
-            value: sliderValue,
-            onChanged: (double value) {
-              setState(() {
-                sliderValue = value;
-              });
-            },
-          ),
-        ],
+            Text(controller.text),
+            Checkbox.adaptive(
+              tristate: true,
+              value: isChenked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChenked = value;
+                });
+              },
+            ),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: Text('Click me'),
+              value: isChenked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChenked = value;
+                });
+              },
+            ),
+            Switch.adaptive(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            SwitchListTile.adaptive(
+              title: Text('Switch me'),
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider.adaptive(
+              min: 0,
+              max: 100,
+              divisions: 10,
+              value: sliderValue,
+              onChanged: (double value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+            ),
+            sliderValue == 0
+                ? Text('Slider is at minimum')
+                : sliderValue == 100
+                ? Text('Slider is at maximum')
+                : Text('Slider value: $sliderValue'),
+            Image.asset('assets/images/bg.jpg'),
+            Image.asset('assets/images/bg.jpg'),
+          ],
+        ),
       ),
     );
   }
