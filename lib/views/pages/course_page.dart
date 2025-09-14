4582013eed402f51ac8988ad2c66db4f67ea5de5
MichailoZaichenko <michailo.zaichenko@gmail.com>
2025-09-14 18:21:04 +0300

    get api

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/views/widgets/container_windget.dart';
 import 'package:first_app/views/widgets/hero_widget.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var itemCount = jsonResponse['activity'];
+      print(itemCount);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/views/pages/expended_flexible_page.dart
+++ lib/views/pages/expended_flexible_page.dart
@@ -7,28 +7,57 @@ class ExtendedFlexiblePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.teal,
-                  height: 20.0,
-                  child: Text("Hello"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Expanded(
+            //   child: Container(
+            //     color: Colors.teal,
+            //     height: 20.0,
+            //     child: Text("Hello"),
+            //   ),
+            // ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.teal,
+                    height: 20.0,
+                    child: Text("Hello"),
+                  ),
                 ),
-              ),
-              Flexible(
-                flex: 2,
-                child: Container(
-                  color: Colors.red,
-                  height: 20.0,
-                  child: Text('Hello'),
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.red,
+                    height: 20.0,
+                    child: Text('Hello'),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            Divider(),
+            Row(
+              children: [
+                Flexible(
+                  flex: 4,
+                  child: Container(
+                    color: Colors.red,
+                    height: 20.0,
+                    child: Text('Hello'),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.teal,
+                    height: 20.0,
+                    child: Text("Hello"),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
